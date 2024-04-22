2f68369828504f006abd617ef615e4372d355d83
roblebob <robert.koppisch@gmail.com>
2024-04-22 11:01:47 +0200

    before lifting up

---
+++ src/components/Summary.jsx
@@ -0,0 +1,14 @@
+// src/components/Summary.jsx
+
+import React from "react";
+
+function Summary(props) {
+  return (
+    <div>
+      <h1>TASKS COMPLETED:</h1>
+      <p className="tasks-completed">0</p>
+    </div>
+  );
+}
+
+export default Summary;

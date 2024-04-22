0ec90832895dbc987c1b7c38a2eb63d9e7854733
roblebob <robert.koppisch@gmail.com>
2024-04-22 11:42:30 +0200

    done

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -6,7 +6,7 @@ function Summary(props) {
   return (
     <div>
       <h1>TASKS COMPLETED:</h1>
-      <p className="tasks-completed">0</p>
+      <p className="tasks-completed">{props.tasksCompleted}</p>
     </div>
   );
 }
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -5,32 +5,58 @@ import Task from "./Task";
 import Summary from "./Summary";
 
 const initialTasks = [
-    {
+  {
     _id: "1a",
     name: "Task1",
     description: "Do something important",
+    isDone: false,
   },
   {
     _id: "2b",
     name: "Task2",
     description: "Do something important",
+    isDone: false,
   },
   {
     _id: "3c",
     name: "Task3",
     description: "Do something important",
+    isDone: false,
   },
 ];
 
 function ToDoList() {
   const [tasks, setTasks] = useState(initialTasks);
+  const [tasksCompleted, setTasksCompleted] = useState(0);
+
+  const toggleTaskDone = (id) => {
+    const tasksCopy = [...tasks];
+
+    tasksCopy.forEach((task) => {
+      if (task._id === id) {
+        // task.isDone = task.isDone ? false : true;
+        task.isDone = !task.isDone;
+        setTasksCompleted(
+          task.isDone ? tasksCompleted + 1 : tasksCompleted - 1
+        );
+        // if (task.isDone) {
+        //   setTasksCompleted(tasksCompleted + 1);
+        // } else if (!task.isDone) {
+        //   setTasksCompleted(tasksCompleted - 1);
+        // }
+      }
+    });
+    setTasks(tasksCopy);
+  };
 
   return (
     <div>
-      <Summary />
-      
+      <Summary tasksCompleted={tasksCompleted} />
+
       <div className="todo-container">
-        {tasks.map((task) => <Task key={task._id} task={task} /> )}
+        {tasks.map((task) => (
+          <Task key={task._id} task={task} toggleTaskDone={toggleTaskDone} />
+        ))}
       </div>
     </div>
   );
